58c5f4c243351ac9cc7e4ba7b1aab73d87f02c8e
dedi <dediirwansyah1925@gmail.com>
2021-08-14 18:07:06 +0700

    mengubah form layout ubah buku

--- src/components/ManajemenBuku.js
+++ src/components/ManajemenBuku.js
@@ -6,8 +6,48 @@ function ManajemenBuku({ bookList }) {
     <div className='container mt-3'>
       <h1 className='text-center'>Manajemen Buku</h1>
 
-      <div id='formTambah'></div>
-      <div id='formUbah'></div>
+      <div id='formTambah'>
+          <h5>Tambah Buku</h5>
+          <hr />
+          <form className="row">
+              <div className="col-3">
+                <input type="text" name="judul" className="form-control mx-2" placeholder="judul"  />
+              </div>
+              <div className="col-3">
+                <input type="text" name="pengarang" className="form-control mx-2" placeholder="pengarang"  />
+              </div>
+              <div className="col-2">
+                <input type="text" name="harga" className="form-control mx-2" placeholder="harga"  />
+              </div>
+              <div className="col-2">
+                <input type="number" name="stok" className="form-control mx-2" placeholder="stok"  />
+              </div>
+              <div className="col-2">
+                <input type="submit" className="btn btn-primary ml-5" placeholder="simpan" />
+              </div>
+            </form>
+      </div>
+      <div id='formUbah' className="mt-3">
+          <h5>Ubah Buku</h5>
+          <hr />
+          <form className="row">
+              <div className="col-3">
+                <input type="text" name="judul" className="form-control mx-2" placeholder="judul" />
+              </div>
+              <div className="col-3">
+                <input type="text" name="pengarang" className="form-control mx-2" placeholder="pengarang" />
+              </div>
+              <div className="col-2">
+                <input type="text" name="harga" className="form-control mx-2" placeholder="harga" />
+              </div>
+              <div className="col-2">
+                <input type="number" name="stok" className="form-control mx-2" placeholder="stok" />
+              </div>
+              <div className="col-2">
+                <input type="submit" className="btn btn-warning ms-2" value="ubah" />
+              </div>
+              </form>
+      </div>
       <div id='daftarBuku'>
         <h2 className='mt-3'>Daftar Buku</h2>
         <hr />
