0ff2753d5f709c46949f94fa586e9399845fa8e8
dedi <dediirwansyah1925@gmail.com>
2021-08-14 16:55:09 +0700

    membuat import boostrap di index.js dan create component manajamen buku

---
+++ src/components/ManajemenBuku.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ManajemenBuku() {
+  return (
+    <div>
+      <div className='container mt-3'>
+        <h1 className='text-center'>Manajemen Buku</h1>
+
+        <div id='formTambah'></div>
+        <div id='formUbah'></div>
+        <div id='daftarBuku'></div>
+      </div>
+    </div>
+  );
+}
+
+export default ManajemenBuku;
