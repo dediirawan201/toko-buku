6192954adc3049f7b609450f535b3fb666f718d5
dedi <dediirwansyah1925@gmail.com>
2021-08-14 16:58:33 +0700

    membuat file beranda.js

---
+++ src/components/Beranda.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Beranda() {
+    return (
+        <div className="mt-3 container w-75">
+            <h1 className="text-center">Selamat datang di toko buku compt 404</h1>
+
+            <div id="katalog"></div>
+        </div>
+    )
+}
+
+export default Beranda;
