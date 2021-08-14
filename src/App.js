b2c0bec62e4b47e9095e370d4ee2648ba8111c9a
dedi <dediirwansyah1925@gmail.com>
2021-08-14 17:21:51 +0700

    membuat data dummy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Beranda from "./components/Beranda";
@@ -6,6 +6,10 @@ import ManajemenBuku from "./components/ManajemenBuku";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [books, setBooks] = useState(
+    {_id:1, judul:"laskar pelangi", pengarang:"andrea hinata", harga:400000, stok:10},
+  {_id:2, judul:"Naruto", pengarang:"laskar", harga:900000, stok:11}
+  )
   return (
     <div>
       <BrowserRouter>
